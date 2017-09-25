5a1a70d0de2df31f6a1409c65592e56679145524
Alfian <piant.grunger@gmail.com>
2017-09-25 13:26:46 +0700

    commit hari ini

--- models/Setting.php
+++ models/Setting.php
@@ -15,6 +15,10 @@ use yii\db\Expression;
  * @property int $id_setting
  * @property string $nama_perusahaan
  * @property string $alamat_perusahaan
+ * @property string $telp_perusahaan1
+ * @property string $telp_perusahaan2
+ * @property string $email_perusahaan
+ * @property string $website_perusahaan
  * @property string $logo_perusahaan
  * @property string $ket
  * @property string $created_at
@@ -52,9 +56,10 @@ class Setting extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['nama_perusahaan', 'alamat_perusahaan'], 'required'],
             [['alamat_perusahaan', 'ket'], 'string'],
             [['created_at', 'updated_at'], 'safe'],
-            [['nama_perusahaan', 'logo_perusahaan'], 'string', 'max' => 50],
+            [['nama_perusahaan', 'telp_perusahaan1', 'telp_perusahaan2', 'email_perusahaan', 'website_perusahaan', 'logo_perusahaan'], 'string', 'max' => 50],
         ];
     }
 
@@ -67,6 +72,10 @@ class Setting extends \yii\db\ActiveRecord
             'id_setting' => Yii::t('app', 'Id Setting'),
             'nama_perusahaan' => Yii::t('app', 'Nama Perusahaan'),
             'alamat_perusahaan' => Yii::t('app', 'Alamat Perusahaan'),
+            'telp_perusahaan1' => Yii::t('app', 'Telp Perusahaan1'),
+            'telp_perusahaan2' => Yii::t('app', 'Telp Perusahaan2'),
+            'email_perusahaan' => Yii::t('app', 'Email Perusahaan'),
+            'website_perusahaan' => Yii::t('app', 'Website Perusahaan'),
             'logo_perusahaan' => Yii::t('app', 'Logo Perusahaan'),
             'ket' => Yii::t('app', 'Ket'),
             'created_at' => Yii::t('app', 'Created At'),
