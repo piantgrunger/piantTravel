e0f10f1fa29b75e5381acb4b479566ed5d407618
Alfian <piant.grunger@gmail.com>
2017-10-05 11:11:05 +0700

    commit hari ini sampe sopir

--- templates/views/layouts/left.php
+++ templates/views/layouts/left.php
@@ -2,19 +2,19 @@
 use hscstudio\mimin\components\Mimin;
 $menuItems =
         [
-                    ['label' => 'Gii','icon' => 'fa fa-file-code-o', 'url' => ['/gii/'],'visible' => !Yii::$app->user->isGuest],
+                    ['label' => 'Gii','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] ,'icon' => 'fa fa-file-code-o', 'url' => ['/gii/'],'visible' => !Yii::$app->user->isGuest],
                     [
                         'visible' => !Yii::$app->user->isGuest,
                         'label' => 'User / Group',
                         'icon' => 'user-circle',
                         'url' => '#',
                         'items' => [
-                    ['label' => 'App. Route', 'icon' =>  'user', 'url' => ['/mimin/route/'],'visible' => !Yii::$app->user->isGuest],
-                    ['label' => 'Role', 'icon' =>  'user', 'url' => ['/mimin/role/'],'visible' => !Yii::$app->user->isGuest],
-                    ['label' => 'User', 'icon' => 'user', 'url' => ['/mimin/user/'],'visible' => !Yii::$app->user->isGuest],
+                    ['label' => 'App. Route','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'user', 'url' => ['/mimin/route/'],'visible' => !Yii::$app->user->isGuest],
+                    ['label' => 'Role','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'user', 'url' => ['/mimin/role/'],'visible' => !Yii::$app->user->isGuest],
+                    ['label' => 'User','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' => 'user', 'url' => ['/mimin/user/'],'visible' => !Yii::$app->user->isGuest],
                    ]]
                         ,
-            ['label' => 'Setting', 'icon' => 'dashboard', 'url' => ['/setting/'],'visible' => !Yii::$app->user->isGuest],
+            ['label' => 'Setting','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' => 'dashboard', 'url' => ['/setting/'],'visible' => !Yii::$app->user->isGuest],
             
             [
                         'visible' => !Yii::$app->user->isGuest,
@@ -22,15 +22,15 @@ $menuItems =
                         'icon' => 'bus',
                         'url' => '#',
                         'items' => [
-                    ['label' => 'Jns Kendaraan', 'icon' =>  'car', 'url' => ['/jnskendaraan/'],'visible' => !Yii::$app->user->isGuest],
-              ['label' => 'Kendaraan', 'icon' =>  'taxi', 'url' => ['/kendaraan/'],'visible' => !Yii::$app->user->isGuest],
-            ['label' => 'Sopir', 'icon' =>  'drivers-license-o', 'url' => ['/sopir/'],'visible' => !Yii::$app->user->isGuest],
+                    ['label' => 'Jns Kendaraan','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'car', 'url' => ['/jnskendaraan/'],'visible' => !Yii::$app->user->isGuest],
+              ['label' => 'Kendaraan','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'taxi', 'url' => ['/kendaraan/'],'visible' => !Yii::$app->user->isGuest],
+            ['label' => 'Sopir','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'drivers-license-o', 'url' => ['/sopir/'],'visible' => !Yii::$app->user->isGuest],
            
                    ]]
                         ,
            
-                   ['label' => 'Customer', 'icon' =>  'user-o', 'url' => ['/customer/'],'visible' => !Yii::$app->user->isGuest],
-                  ['label' => 'Paket', 'icon' =>  'money', 'url' => ['/paket/'],'visible' => !Yii::$app->user->isGuest],
+                   ['label' => 'Customer','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'user-o', 'url' => ['/customer/'],'visible' => !Yii::$app->user->isGuest],
+                  ['label' => 'Paket','options' => ['class' => 'treeview' ,'data-target'=>"#" ,"data-toggle"=>"pill" ] , 'icon' =>  'money', 'url' => ['/paket/'],'visible' => !Yii::$app->user->isGuest],
            
             
                 ];     
